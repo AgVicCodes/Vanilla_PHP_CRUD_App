0f6b29e6bb9b3df478d8ee001d7bbb832808c3c0
AgVicCodes <victoragbeleye2@gmail.com>
2023-11-04 13:47:39 +0100

    Modified Workspace

--- advanced.php
+++ advanced.php
@@ -1,7 +1,16 @@
 <?php
 
     // Php dates
-    echo date("l");
-
+    /*
+    d - Day of the month 01 to 31.
+    m - month of the year 01 - 12
+    y - year. 4 numbers. (Capital letter indicates full year)
+    l - Day of the week. Mon to Fri.
+    */
+    echo date("l") . " " . date("d") . "-" . date("m") . "-" . date("y");
 
 ?>
+
+<footer>
+    Copyright &copy 2020-<?= date("Y") ?>. All rights reserved!
+</footer>
