2e33cdb71281f6e501bc462759d36f07b3446336
AgVicCodes <victoragbeleye2@gmail.com>
2023-11-07 22:25:19 +0100

    Modified Workspace

---
+++ fileUpload.php
@@ -0,0 +1,20 @@
+<?php 
+
+    // file upload
+
+    /* 
+        1. Add vars - $file, $uploadpath, $uploadok
+        2. File upload script
+        3. Check if file type is valid
+        4. Check if file size is ok
+        5. Check if file is a duplicate.
+        
+        Correction:
+        1. $file = $target_dir, $uploadpath = $target_file, 
+           Missing - $imageFileType.
+        2-5. Correct
+    */
+
+    
+
+?>
