c02093132b3a02694c473182179b2ca77662a10e
AgVicCodes <victoragbeleye2@gmail.com>
2023-11-11 04:04:34 +0100

    Modified Workspace

--- fileUpload.php
+++ fileUpload.php
@@ -94,43 +94,11 @@
         </style>
     </head>
     <body>
-        <form action="" enctype="multipart/form-data">
+        <form action="./upload.php" enctype="multipart/form-data">
             <input type="file" name="file" id=file>
             <img src="" alt="">
-            <input type="submit" name="submit" id="submit">
+            <input type="submit" id="submit">
         </form>
     </body>
 </html>
 
-<?php 
-
-    // file upload
-
-    /* 
-        1. Add vars - $target_dir, $uploadpath, $uploadOk
-        2. File upload script
-        3. Check if file type is valid
-        4. Check if file size is ok
-        5. Check if file is a duplicate.
-
-        Add attr enctype="multipart/form-data"
-        
-        Correction:
-        1. $target_dir = $target_dir, $uploadpath = $target_file, 
-           Missing - $imageFileType.
-        2-5. Correct
-    */
-
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-    // To check if the file is an image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        
-    }
-
-
-?>
---
+++ upload.php
@@ -0,0 +1,120 @@
+<?php 
+
+    // file upload
+
+    /* 
+        1. Add vars - $target_dir, $uploadpath, $uploadOk
+        2. File upload script
+        3. Check if file type is valid
+        4. Check if file size is ok
+        5. Check if file is a duplicate.
+
+        Add attr enctype="multipart/form-data"
+        
+        Correction:
+        1. $target_dir = $target_dir, $uploadpath = $target_file, 
+           Missing - $imageFileType.
+        2-5. Correct
+    */
+
+    // w3schools
+
+    $target_dir = "uploads/";
+    $target_file = ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // GeeksforGeeks;
+    
+    /* $target_dir = $_POST["uploads"]."/"; 
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]); 
+    $uploadOk = 1; 
+  
+    if($_SERVER["REQUEST_METHOD"] == "POST") { 
+    
+        // To check whether directory exist or not 
+        if(!empty($_POST["dirname"])) { 
+            if(!is_dir($_POST["dirname"])) {     
+                mkdir($_POST["dirname"]);     
+                $uploadOk = 1;             
+            } 
+        } 
+        else { 
+            echo "Specify the directory name..."; 
+            $uploadOk = 0; 
+            exit;     
+        } 
+    
+        // Check if file was uploaded without errors 
+        if(isset($_FILES["fileToUpload"]) &&  
+            $_FILES["fileToUpload"]["error"] == 0) { 
+            $allowed_ext = array("jpg" => "image/jpg", 
+                                "jpeg" => "image/jpeg", 
+                                "gif" => "image/gif", 
+                                "png" => "image/png"); 
+            $file_name = $_FILES["fileToUpload"]["name"]; 
+            $file_type = $_FILES["fileToUpload"]["type"]; 
+            $file_size = $_FILES["fileToUpload"]["size"]; 
+        
+            // Verify file extension 
+            $ext = pathinfo($file_name, PATHINFO_EXTENSION); 
+    
+            if (!array_key_exists($ext, $allowed_ext)) { 
+                die("Error: Please select a valid file format."); 
+            }     
+                
+            // Verify file size - 2MB max 
+            $maxsize = 2 * 1024 * 1024; 
+            
+            if ($file_size > $maxsize) { 
+                die("Error: File size is larger than the allowed limit."); 
+            }                     
+        
+            // Verify MYME type of the file 
+            if (in_array($file_type, $allowed_ext)) 
+            { 
+                // Check whether file exists before uploading it 
+                if (file_exists("upload/" . $_FILES["fileToUpload"]["name"])) { 
+                    echo $_FILES["fileToUpload"]["name"]." is already exists."; 
+                }         
+                else { 
+                    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"],  
+                    $target_file)) { 
+                        echo "The file ".  $_FILES["fileToUpload"]["name"].  
+                        " has been uploaded."; 
+                    }  
+                    else { 
+                        echo "Sorry, there was an error uploading your file."; 
+                    } 
+                } 
+            } 
+            else { 
+                echo "Error: Please try again."; 
+            } 
+        } 
+        else { 
+            echo "Error: ". $_FILES["fileToUpload"]["error"]; 
+        } 
+    } */
+
+?>
